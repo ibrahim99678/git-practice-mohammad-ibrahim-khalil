477cba3dae0fc3601e9b80acd3e3a6640c5fcd65
Mohammad Ibrahim khalil <ibrahim.cse10@gmail.com>
2026-08-02 16:47:36 +0600

    Add basic error handling to divide function

--- src/main.py
+++ src/main.py
@@ -5,7 +5,15 @@ def main():
     print("Name: Mohamamd Ibrahim khalil")
     print(f"Todays Date: {datetime.date.today()}")
 
+    print(f"Addition: {utils.add(10, 5)}")
+    print(f"Subtraction: {utils.subtract(15, 5)}")
+    print(f"Multiplication: {utils.multiply(10, 5)}")
 
-print("\n == CALCULATOR ==")
-print(f"10+5 ={utils.add(10,5)}")
-print(f"15-5 ={utils.subtract(15-5)}")
+    try:
+        print(f"10 / 0 = {utils.divide(10, 0)}")
+    except ValueError as e:
+        print(f"Error handling test: {e}")
+
+if __name__ == "__main__":
+    main()
+
--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ def subtract(a,b):
 def multiply(a,b):
     return a*b
 
-def divide(a,b):
+def divide(a, b):
     if b == 0:
-        return "Error: Division by zero"
-    return a/b
+        raise ValueError("Cannot divide by zero.")
+    return a / b
