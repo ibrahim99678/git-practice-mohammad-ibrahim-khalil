46c4613bc5df1d8d715ce94e1c9537b60270cc8d
Mohammad Ibrahim khalil <ibrahim.cse10@gmail.com>
2026-08-02 16:14:34 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import datetime
+import utils
 
 def main():
     print("Name: Mohamamd Ibrahim khalil")
     print(f"Todays Date: {datetime.date.today()}")
 
-    
+
+print("\n == CALCULATOR ==")
+print(f"10+5 ={utils.add(10,5)}")
+print(f"15-5 ={utils.subtract(15-5)}")
